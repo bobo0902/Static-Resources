cee9d86c3783e42dc9afceed5fc5c6f740b6dca3
bobo <625391250@qq.com>
2018-12-05 20:55:15 +0800

    remove cpmponent import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
-import { DatePickerComponent } from './common/date-picker/date-picker/date-picker.component';
-import { TreeSelectComponent } from './common/tree-select/tree-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DatePickerComponent,
-    TreeSelectComponent
+    AppComponentt
   ],
   imports: [
     NgZorroAntdModule,
